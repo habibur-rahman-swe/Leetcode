80d3be687c9a17676e66ebae658f74740103e47a
Habibur Rahman <58985776+Habibur0155@users.noreply.github.com>
2022-06-13 22:59:44 +0600

    Time: 6 ms (40.44%), Space: 43.9 MB (58.74%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        Arrays.sort(nums);
+        int cnt = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i-1] == nums[i]) ++cnt;
+            else {
+                if (cnt < 3) return nums[i-1];
+                cnt = 1;
+            }
+        }
+        return nums[n-1];
+    }
+}
