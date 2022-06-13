4f41a6a691f1e47eb170f699d1b047c558a81486
Habibur Rahman <58985776+Habibur0155@users.noreply.github.com>
2022-06-13 20:21:41 +0600

    Time: 28 ms (92.91%), Space: 111.9 MB (70.73%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -1,19 +1,23 @@
 class Solution {
-    public int maximumUniqueSubarray(int[] nums) {
-        int n = nums.length;
-        int[] sum = new int[n+1];
-        for (int i = 0; i < n; i++) {
-            sum[i+1] = sum[i] + nums[i];
-        }
-        int ans = 0;
-        int idx = 0;
-        int[] M = new int[100001];
-        Arrays.fill(M, -1);
-        for (int i = 0; i < n; i++) {
-            idx = Math.max(idx, M[nums[i]] + 1);
-            ans = Math.max(ans, sum[i+1] - sum[idx]);
-            M[nums[i]] = i;
-        }
+    public int maximumUniqueSubarray(int[] arr) {
+        int n = arr.length;
+        int[] idx = new int[100005];
+		Arrays.fill(idx, -1);
+		int[] sum = new int[n];
+		sum[0] = arr[0];
+		for (int i = 1; i < n; i++) sum[i] = sum[i-1] + arr[i];
+		int ans = 0;
+		int mxIdx = -1;
+		for (int i = 0; i < n; i++) {
+			if (mxIdx == -1 && idx[arr[i]] == -1) {
+                ans = Math.max(ans, sum[i]);
+                idx[arr[i]] = i;
+            } else {
+                mxIdx = Math.max(idx[arr[i]], mxIdx);
+                ans = Math.max(ans, sum[i] - sum[mxIdx]);
+                idx[arr[i]] = i;
+            }
+		}
         return ans;
     }
 }
