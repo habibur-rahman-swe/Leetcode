5833d27f227fe5b2832d91156db39e3990f59806
Habibur Rahman <58985776+Habibur0155@users.noreply.github.com>
2022-06-16 00:01:25 +0600

    Time: 102 ms (15.15%), Space: 62.7 MB (45.21%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int longestStrChain(String[] str) {
+        int n = str.length;
+        int[] ans = new int[n];
+		Arrays.fill(ans, 1);
+		Arrays.sort(str, (a, b)->Integer.compare(a.length(), b.length()));
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (str[j].length() + 1 == str[i].length() && diff(str[j], str[i])) {
+					ans[i] = Math.max(ans[i], ans[j] + 1);
+				}
+			}
+		}
+		
+		int max = 0;
+		
+		for (int x : ans) {
+			max = Math.max(max, x);
+		}
+        return max;
+    }
+    private boolean diff(String s1, String s2) {
+		char[] c1 = s1.toCharArray();
+		char[] c2 = s2.toCharArray();
+		
+		int l1 = c1.length;
+		int l2 = c2.length;
+		
+		int i = 0, j = 0;
+		
+		while (i < l1 && j < l2) {
+			if (c1[i] == c2[j]) {
+				++i;
+				++j;
+			} else {
+				++j;
+			}
+		}
+		
+		return (i == l1 && j == l2) || (i == j);
+	}
+}
