a9470912c940ab79a6bd409b419e635202aef426
Habibur Rahman <58985776+Habibur0155@users.noreply.github.com>
2022-06-17 01:19:01 +0600

    Time: 2 ms (86.90%), Space: 42.8 MB (62.69%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int myAtoi(String s) {
+        long num = 0;
+        int plus = 0;
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        
+        for (char c : s.toCharArray()) {
+            if (c == ' ' && plus == 0) {
+                continue;
+            } else if (c >= '0' && c <= '9') {
+                num = num * 10 + c - '0';
+                if (plus == 0) plus = 1;
+            } else if (c == '-' || c == '+') {
+                if (plus != 0) break;
+                if (c == '-') {
+                    plus = -1;
+                }
+                else plus = 1;
+            } else {
+                break;
+            }
+            if (num * plus > max) return max;
+            if (num * plus < min) return min;
+        }
+        return (int) num * plus;
+    }
+}
