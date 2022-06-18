72691561f3934abcb1cda549b06d2e2c9172b7d8
Habibur Rahman <58985776+Habibur0155@users.noreply.github.com>
2022-06-19 02:04:04 +0600

    Time: 19 ms (96.25%), Space: 42 MB (97.60%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -5,10 +5,15 @@ class Solution {
         int n = s.length();
         int start = 0, end = 0;
         String res = "";
+        int tl = 0, tr = 0;
         
         for (int i = 0; i < n; i++) {
             left = i;
             right = i;
+            
+            tl = i - (end - start)/2;
+            tr = i + (end - start)/2;
+            if (tl >= 0 && tr < n && s.charAt(tl) == s.charAt(tr))
             while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
                 left--;
                 right++;
@@ -19,7 +24,9 @@ class Solution {
             }
             left = i;
             right = i + 1;
-            if (right < n && s.charAt(right) != s.charAt(left)) continue;
+            tl = i - (end - start)/2;
+            tr = i + (end - start)/2+1;
+            if (tl >= 0 && tr < n && s.charAt(tl) == s.charAt(tr))
             while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
                 left--;
                 right++;
