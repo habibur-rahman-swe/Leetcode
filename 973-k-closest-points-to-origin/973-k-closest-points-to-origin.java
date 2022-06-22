befbc29fcc2472fd4a5a9c64935374337e475229
Habibur Rahman <58985776+Habibur0155@users.noreply.github.com>
2022-06-23 00:03:41 +0600

    Time: 160 ms (9.60%), Space: 118.2 MB (34.68%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x, y) -> hm.get(y) - hm.get(x));
+        
+        for (int i = 0; i < points.length; i++) {
+            int d = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            hm.put(i, d);
+            pq.add(i);
+        }
+        
+        while (pq.size() > k) pq.poll();
+        
+        int s = pq.size();
+        int[][] res = new int[s][2];
+        int idx = 0;
+        for (int x : pq) {
+            res[idx][0] = points[x][0];
+            res[idx][1] = points[x][1];
+            idx++;
+        }
+        
+        return res;
+    }
+}
