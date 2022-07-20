ddc3093bbdf01f0e95d4e33c5ae17e094fd0d76c
Habibur Rahman <58985776+Habibur0155@users.noreply.github.com>
2022-07-20 09:10:35 +0600

    Time: 165 ms (49.88%), Space: 54.1 MB (83.18%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+        int cnt = 0;
+        
+        HashSet<String> has = new HashSet<>();
+        HashSet<String> not = new HashSet<>();
+        
+        for (String w : words) {
+            int i = 0, j = 0;
+            
+            if (has.contains(w)) {
+                ++cnt;
+                continue;
+            }
+            
+            if (not.contains(w)) {
+                continue;
+            }
+            
+            while (i < s.length() && j < w.length()) {
+                if (s.charAt(i) == w.charAt(j)) {
+                    j++;
+                }
+                i++;
+            }
+            
+            if (j == w.length()) {
+                ++cnt;
+                has.add(w);
+            } else {
+                not.add(w);
+            }
+        }
+        return cnt;
+    }
+}
