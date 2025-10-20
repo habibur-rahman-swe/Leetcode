c95c6df0dd8964f18aa312598f0b2c9264ea19c3
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:33:23 +0600

    Create 2825-MinimizeStringLength

---
+++ 2825-MinimizeStringLength/2825-MinimizeStringLength.java
@@ -0,0 +1,10 @@
+// Last updated: 10/20/2025, 11:33:23 AM
+class Solution {
+    public int minimizedStringLength(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+        return set.size();
+    }
+}
