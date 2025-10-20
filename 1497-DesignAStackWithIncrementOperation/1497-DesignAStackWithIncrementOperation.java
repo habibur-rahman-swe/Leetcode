f4120a840e564f359986f86be5e859babfcdf3b3
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:39:20 +0600

    Create 1497-DesignAStackWithIncrementOperation

---
+++ 1497-DesignAStackWithIncrementOperation/1497-DesignAStackWithIncrementOperation.java
@@ -0,0 +1,53 @@
+// Last updated: 10/20/2025, 11:39:19 AM
+class CustomStack {
+    private int[] stackArray;
+    private int[] incrementArray;
+    private int topIndex;
+    private int maxSize;
+
+    public CustomStack(int maxSize) {
+        stackArray = new int[maxSize];
+        incrementArray = new int[maxSize];
+        topIndex = -1;
+        this.maxSize = maxSize;
+    }
+    
+    public void push(int x) {
+        if (topIndex < maxSize - 1) {
+            stackArray[++topIndex] = x;
+        }
+    }
+    
+    public int pop() {
+        if (topIndex < 0) {
+            return -1;
+        }
+
+        int result = stackArray[topIndex] + incrementArray[topIndex];
+        
+        if (topIndex > 0) {
+            incrementArray[topIndex - 1] += incrementArray[topIndex];
+        }
+        
+        incrementArray[topIndex] = 0;
+
+        --topIndex;
+        
+        return result;
+    }
+    
+    public void increment(int k, int val) {
+        if (topIndex >= 0) {
+            int incrementIndex = Math.min(topIndex, k - 1);
+            incrementArray[incrementIndex] += val;
+        }
+    }
+}
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack obj = new CustomStack(maxSize);
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * obj.increment(k,val);
+ */
