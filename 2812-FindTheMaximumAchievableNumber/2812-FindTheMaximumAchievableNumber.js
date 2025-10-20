fba18ee4c643709c0d627868e6e74931948e63f9
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:33:27 +0600

    Create 2812-FindTheMaximumAchievableNumber

---
+++ 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.js
@@ -0,0 +1,9 @@
+// Last updated: 10/20/2025, 11:33:27 AM
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num + t * 2;
+};
