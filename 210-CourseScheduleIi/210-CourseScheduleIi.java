673ab84c668701c4725968d36d1453511fffaf70
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:45:31 +0600

    Create 210-CourseScheduleIi

---
+++ 210-CourseScheduleIi/210-CourseScheduleIi.java
@@ -0,0 +1,43 @@
+// Last updated: 10/20/2025, 11:45:31 AM
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
+
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] prerequisite : prerequisites) {
+            adj.get(prerequisite[1]).add(prerequisite[0]);
+            indegree[prerequisite[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int[] order = new int[numCourses];
+
+        int nodesVisited = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            nodesVisited++;
+            order[nodesVisited - 1] = node;
+
+            for (int neighbor : adj.get(node)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        if (nodesVisited != numCourses) {
+            order = new int[]{};
+        }
+        return order;
+    }
+}
