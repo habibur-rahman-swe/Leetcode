832b6c102728358898accc2b63ceb4a27a59255c
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:36:41 +0600

    Create 2149-RemoveColoredPiecesIfBothNeighborsAreTheSameColor

---
+++ 2149-RemoveColoredPiecesIfBothNeighborsAreTheSameColor/2149-RemoveColoredPiecesIfBothNeighborsAreTheSameColor.java
@@ -0,0 +1,14 @@
+// Last updated: 10/20/2025, 11:36:41 AM
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        int a = 0, b = 0;
+        for (int i = 1; i < colors.length() - 1; i++) {
+            if (colors.charAt(i) == colors.charAt(i-1) && colors.charAt(i) == colors.charAt(i+1)) {
+                if (colors.charAt(i) == 'A') ++a;
+                else ++b;
+            }
+        }
+
+        return a > b;
+    }
+}
