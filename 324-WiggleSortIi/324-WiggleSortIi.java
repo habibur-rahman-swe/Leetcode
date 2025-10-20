f8137cde7eb2a24f585c282b2bb3208c2a9a4451
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:44:50 +0600

    Create 324-WiggleSortIi

---
+++ 324-WiggleSortIi/324-WiggleSortIi.java
@@ -0,0 +1,24 @@
+// Last updated: 10/20/2025, 11:44:50 AM
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        
+        Arrays.sort(nums);
+        
+        int[] arr = new int[n];
+        int e = (n-1)/2, o = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (i%2==0) {
+                arr[i] = nums[e];
+                e--;
+            } else {
+                arr[i] = nums[o];
+                o--;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            nums[i] = arr[i];
+        }
+    }
+}
