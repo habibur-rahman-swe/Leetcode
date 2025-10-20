741d6a3fc52dae868859dde81e4e922d4c251233
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:36:49 +0600

    Create 2122-CountSpecialQuadruplets

---
+++ 2122-CountSpecialQuadruplets/2122-CountSpecialQuadruplets.java
@@ -0,0 +1,19 @@
+// Last updated: 10/20/2025, 11:36:48 AM
+class Solution {
+    public int countQuadruplets(int[] nums) {
+        int n = nums.length;
+        //Arrays.sort(nums);
+        
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        if (nums[i] + nums[j] + nums[k] == nums[l]) ++ans;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
