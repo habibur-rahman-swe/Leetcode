ed553e5f046f99120f377efefc2c6d44ed065bf0
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:41:09 +0600

    Create 998-CheckCompletenessOfABinaryTree

---
+++ 998-CheckCompletenessOfABinaryTree/998-CheckCompletenessOfABinaryTree.java
@@ -0,0 +1,40 @@
+// Last updated: 10/20/2025, 11:41:09 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        if (root == null) return true;
+
+        boolean nullNodeFound = false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            if (node == null) {
+                nullNodeFound = true;
+            } else {
+                if (nullNodeFound) {
+                    return false;
+                }
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+        return true;
+    }
+}
