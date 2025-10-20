f36023d4f02339bab487600ed69b151f158d176c
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:38:37 +0600

    Create 1635-NumberOfGoodPairs

---
+++ 1635-NumberOfGoodPairs/1635-NumberOfGoodPairs.java
@@ -0,0 +1,13 @@
+// Last updated: 10/20/2025, 11:38:37 AM
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j])++ans;
+            }
+        }
+        
+        return ans;
+    }
+}
