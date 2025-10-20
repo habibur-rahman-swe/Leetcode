a82af2caa24872426a92d88c641bf4a84a059e53
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:43:10 +0600

    Create 620-NotBoringMovies

---
+++ 620-NotBoringMovies/620-NotBoringMovies.sql
@@ -0,0 +1,3 @@
+-- Last updated: 10/20/2025, 11:43:10 AM
+# Write your MySQL query statement below
+select id, movie, description, rating from cinema where mod(id, 2) = 1 and description != 'boring' order by rating desc;
