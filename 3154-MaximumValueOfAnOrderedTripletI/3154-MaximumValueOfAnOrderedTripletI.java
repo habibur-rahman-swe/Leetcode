7d4f53e2e8758464273c48c7f01bd0c18983bfef
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:32:48 +0600

    Create 3154-MaximumValueOfAnOrderedTripletI

---
+++ 3154-MaximumValueOfAnOrderedTripletI/3154-MaximumValueOfAnOrderedTripletI.java
@@ -0,0 +1,15 @@
+// Last updated: 10/20/2025, 11:32:48 AM
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        long res = 0;
+        for (int k = 2; k < n; k++) {
+            int maxPrefix = nums[0];
+            for (int j = 1; j < k; j++) {
+                res = Math.max(res, (long) (maxPrefix - nums[j]) * nums[k]);
+                maxPrefix = Math.max(maxPrefix, nums[j]);
+            }
+        }
+        return res;
+    }
+}
