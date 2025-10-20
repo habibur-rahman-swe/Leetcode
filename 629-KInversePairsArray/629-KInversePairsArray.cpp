67a43fb8b5a137d0686a9b614da320ab6d9c8030
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:43:06 +0600

    Create 629-KInversePairsArray

---
+++ 629-KInversePairsArray/629-KInversePairsArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 10/20/2025, 11:43:06 AM
+class Solution {
+public:
+    int dp[1001][1001];
+    int kInversePairs(int n, int k) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                if (j == 0)
+                    dp[i][j] = 1;
+                else {
+                    for (int p = 0; p <= min(j, i - 1); p++)
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - p]) % 1000000007;
+                }
+            }
+        }
+        return dp[n][k];
+    }
+};
