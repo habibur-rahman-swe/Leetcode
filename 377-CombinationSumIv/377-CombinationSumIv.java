eff8687f747ab20e0695f3e7579f9cff41655ba3
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:44:25 +0600

    Create 377-CombinationSumIv

---
+++ 377-CombinationSumIv/377-CombinationSumIv.java
@@ -0,0 +1,17 @@
+// Last updated: 10/20/2025, 11:44:25 AM
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        
+        for (int i = 1; i <= target; i++) {
+            for (int x : nums) {
+                if (i >= x) {
+                    dp[i] += dp[i-x];
+                }
+            }
+        }
+        
+        return dp[target];
+    }
+}
