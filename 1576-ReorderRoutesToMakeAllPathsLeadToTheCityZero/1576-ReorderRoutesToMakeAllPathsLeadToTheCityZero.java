a3d2d227c32f6f4c1afdf78e0b65f0bdf574c1ac
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:38:55 +0600

    Create 1576-ReorderRoutesToMakeAllPathsLeadToTheCityZero

---
+++ 1576-ReorderRoutesToMakeAllPathsLeadToTheCityZero/1576-ReorderRoutesToMakeAllPathsLeadToTheCityZero.java
@@ -0,0 +1,30 @@
+// Last updated: 10/20/2025, 11:38:55 AM
+class Solution {
+    int count = 0;
+    private void dfs(int node, int parent, Map<Integer, List<List<Integer>>> adj) {
+        if (!adj.containsKey(node)) {
+            return;
+        }
+
+        for (List<Integer> nei : adj.get(node)) {
+            int child = nei.get(0);
+            int sign = nei.get(1);
+            if (child != parent) {
+                count += sign;
+                dfs(child, node, adj);
+            }
+        }
+    }
+
+    public int minReorder(int n, int[][] connections) {
+        Map<Integer, List<List<Integer>>> adj = new HashMap<>();
+
+        for (int[] connection : connections) {
+            adj.computeIfAbsent(connection[0], k -> new ArrayList<List<Integer>>()).add(Arrays.asList(connection[1], 1));
+
+            adj.computeIfAbsent(connection[1], k -> new ArrayList<List<Integer>>()).add(Arrays.asList(connection[0], 0));
+        }
+        dfs(0, -1, adj);
+        return count;
+    }
+}
