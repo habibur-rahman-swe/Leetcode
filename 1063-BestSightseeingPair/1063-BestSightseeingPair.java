36a4aa1b3ee6d5a0ee88f49fa01ac41bb4ed789e
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:40:44 +0600

    Create 1063-BestSightseeingPair

---
+++ 1063-BestSightseeingPair/1063-BestSightseeingPair.java
@@ -0,0 +1,21 @@
+// Last updated: 10/20/2025, 11:40:44 AM
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int n = values.length;
+        int maxLeftScore = values[0];
+
+        int maxScore = 0;
+
+        for (int i = 1; i < n; i++) {
+            int currentRightScore = values[i] - i;
+
+            maxScore = Math.max(maxScore, maxLeftScore + currentRightScore);
+
+            int currentLeftScore = values[i] + i;
+
+            maxLeftScore = Math.max(maxLeftScore, currentLeftScore);
+        }
+
+        return maxScore;
+    }
+}
