ebbd3e5a8391f089e42f48915666ccef3b788768
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:47:59 +0600

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -0,0 +1,28 @@
+// Last updated: 10/20/2025, 11:47:59 AM
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> list = new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+
+        for (int[] interval : intervals) {
+            pq.add(interval);
+        }
+        int start = -1, end = -1;
+        while (!pq.isEmpty()) {
+            int[] interval = pq.poll();
+            if (start <= interval[0] && interval[0] <= end) {
+                end = Math.max(end, interval[1]);
+            } else {
+                list.add(new int[] {start, end});
+                start = interval[0];
+                end = interval[1];
+            }
+        }
+        list.add(new int[] {start, end});
+        int[][] merge = new int[list.size()-1][2];
+        for (int i = 1; i < list.size(); i++) {
+            merge[i-1] = list.get(i);
+        }
+        return merge;
+    }
+}
