f0b752b1755c8978b1db8fe9dccb1dbef122d425
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:39:23 +0600

    Create 1479-ConstructTargetArrayWithMultipleSums

---
+++ 1479-ConstructTargetArrayWithMultipleSums/1479-ConstructTargetArrayWithMultipleSums.java
@@ -0,0 +1,25 @@
+// Last updated: 10/20/2025, 11:39:23 AM
+class Solution {
+    public boolean isPossible(int[] a) {
+        int sum = 0;
+			
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y)->y - x);
+        for (int x : a) {
+            pq.add(x);
+            sum += x;
+        }
+
+        while (pq.peek() > 1) {
+            int x = pq.poll();
+
+            sum -= x;
+            x -= sum;
+            while (x > 0 && sum > 0 && x > sum) x -= sum;
+            if (sum <= 0 || x <= 0) return false;
+            sum += x;
+            pq.add(x);
+        }
+        for (int x : pq) if (x != 1) return false;
+        return true;
+    }
+}
