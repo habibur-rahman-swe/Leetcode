5312dc49963c9ead9f0fa71b2a9523dd09e69ce4
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:48:28 +0600

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,15 @@
+// Last updated: 10/20/2025, 11:48:28 AM
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1; 
+        
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) r = mid - 1;
+            else l = mid + 1;
+        }
+        
+        return l;
+    }
+}
