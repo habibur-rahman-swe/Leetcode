03a3efdb373307ca2b9769c349ca2511ee6f5026
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:46:13 +0600

    Create 149-MaxPointsOnALine

---
+++ 149-MaxPointsOnALine/149-MaxPointsOnALine.java
@@ -0,0 +1,18 @@
+// Last updated: 10/20/2025, 11:46:13 AM
+class Solution {
+    public int maxPoints(int[][] points) {
+        int n = points.length;
+        if (n < 3) return n;
+        int result = 2;
+        for (int i = 0; i < n; i++) {
+            Map<Double, Integer> cnt = new HashMap<>();
+            for (int j = 0; j < n; j++) {
+                if (j != i) {
+                    cnt.merge(Math.atan2(points[j][1] - points[i][1], points[j][0] - points[i][0]), 1, Integer::sum);
+                }
+            }
+            result = Math.max(result, Collections.max(cnt.values()) + 1);
+        }
+        return result;
+    }
+}
