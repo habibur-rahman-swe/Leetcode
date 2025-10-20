dbff332f3757a52bfa4d9d53f8c17b6e0608756d
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:48:52 +0600

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.c
@@ -0,0 +1,27 @@
+// Last updated: 10/20/2025, 11:48:52 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+    struct ListNode* head = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* tail = head;
+    
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->val < l2->val) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = l1 ? l1 : l2;
+    return head->next;
+} 
