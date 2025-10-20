f18ad86901dafa1d8cfe8779652b67747188ab0a
Habibur Rahman <habibur0199mhr@gmail.com>
2025-10-20 11:34:05 +0600

    Create 2696-TheNumberOfBeautifulSubsets

---
+++ 2696-TheNumberOfBeautifulSubsets/2696-TheNumberOfBeautifulSubsets.java
@@ -0,0 +1,29 @@
+// Last updated: 10/20/2025, 11:34:05 AM
+class Solution {
+    public int beautifulSubsets(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        Arrays.sort(nums);
+        return countBeautifulSubsets(nums, k, freqMap, 0) - 1;
+    }
+
+    private int countBeautifulSubsets(int[] nums, int difference, Map<Integer, Integer> freqMap, int i) {
+        if (i == nums.length) {
+            return 1;
+        }
+
+        int totalCount = countBeautifulSubsets(nums, difference, freqMap, i + 1);
+
+        if (!freqMap.containsKey(nums[i] - difference)) {
+            freqMap.put(nums[i], freqMap.getOrDefault(nums[i], 0) + 1);
+
+            totalCount += countBeautifulSubsets(nums, difference, freqMap, i + 1);
+
+            freqMap.put(nums[i], freqMap.get(nums[i]) - 1);
+
+            if (freqMap.get(nums[i]) == 0) {
+                freqMap.remove(nums[i]);
+            }
+        }
+        return totalCount;
+    }
+}
